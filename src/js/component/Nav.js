85ded0c6f12a430770a12f9b8cb8597ef75aa228
Norbert305 <sealsnorbert@yahoo.com>
2022-04-26 23:03:06 +0000

    Nav

--- src/js/component/Nav.js
+++ src/js/component/Nav.js
@@ -6,11 +6,19 @@ export function Nav() {
 	return (
 		<div>
 			<NavBar>
+				<br />
+				<br />
 				<ul>
+					<li style={{ float: "left" }}>
+						<img
+							src="https://s3-alpha-sig.figma.com/img/1e5c/5cc8/d76719f808938a8e31c3808d5b018422?Expires=1652054400&Signature=WntB0OpIdU9v1BGKO8wD4A9wFiy8Nzu5o1LFvIh8ois-Mvn9i4-6ublSdKkEgXdU2CLpXKJBTRdESX2p9iS~-ExdwGBSi9peS1dbNy8KSiYLimAm3DWXhbfA5IsE8fTih6OFMKn~tl-ler-6lJ~gdLl6o3RLfZdLdxo6Sk29-dMyQeb9MlqEeGXTdrJVvFy1QP4d8igMOELIyf9Jyi2vqDBLfhoEKBLvEv9CnC9ihy7AY1Kh2sS4SjMncPnvyXPXpj7PfMb-SflgMDxIg~F2aRSkcUum22zpKEiM5u6n4L~YquqroeuIsD5tGzdWEzPOhaXqAkADnBNmMT7ymenLbw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+							alt="infinity"
+						/>
+					</li>
 					<li style={{ float: "left" }}>
 						<a href="">
 							{" "}
-							<h2>Kazmos</h2>{" "}
+							<h2>KAZMOS</h2>{" "}
 						</a>
 					</li>
 					<li>
--- src/js/component/styledComponents/Nav.styled.js
+++ src/js/component/styledComponents/Nav.styled.js
@@ -8,7 +8,7 @@ ul {
     padding: 0;
   }
 
-  li, h3 {
+  li, h2, img {
     display: inline;
   }
 
@@ -17,6 +17,21 @@ ul {
       float right;
       padding: 8px;
       color: white;
+      text-decoration: none;
+  }
+
+  h2 {
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 59px;
+    font-family: 'Montserrat';
+    font-style: normal;
+  }
+
+  img {
+    width: 86px;
+    height: 56px;
+    margin: 8px 20px;
   }
 
 font-size: 16px;
